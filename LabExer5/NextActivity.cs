f8326c39f75d30bfdfbe5861e6b0cc95cef2317d
Michiru Suzumiya <mamamauag@live.mcl.edu.ph>
2022-06-07 00:50:28 +0800

    fixed bug on update record (functioning)

--- LabExer5/NextActivity.cs
+++ LabExer5/NextActivity.cs
@@ -24,6 +24,7 @@ namespace LabExer5
         HttpWebResponse nextResponse;
         HttpWebRequest nextRequest;
         string name = "", school = "", country = "", selectedGender = "", nameToBeSearched = "", res = "", str = "", login_name = "";
+        int record_ID = 0;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -65,20 +66,46 @@ namespace LabExer5
 
         public void AddRecord(object sender, EventArgs e)
         {
-            name = editName.Text;
-            school = editSchool.Text;
-            country = autoCompleteCountry.Text;
+            if (editName.Text == "" ||
+                editSchool.Text == "" ||
+                autoCompleteCountry.Text == "")
+            {
+                Toast.MakeText(this, "FAILED: Fill out the missing fields and try again.", ToastLength.Long).Show();
+            }
+            else
+            {
+                name = editName.Text;
+                school = editSchool.Text;
+                country = autoCompleteCountry.Text;
 
-            nextRequest = (HttpWebRequest)WebRequest.Create("http://192.168.1.130/IT140P/REST/add_record.php?name=" + name + " &school=" + school + " &country=" + country + " &gender=" + selectedGender);
-            nextResponse = (HttpWebResponse)nextRequest.GetResponse();
-            StreamReader reader = new StreamReader(nextResponse.GetResponseStream());
-            res = reader.ReadToEnd();
-            Toast.MakeText(this, res, ToastLength.Long).Show();
+                try
+                {
+                    nextRequest = (HttpWebRequest)WebRequest.Create("http://192.168.1.130/IT140P/REST/search_record.php?name=" + name);
+                    nextResponse = (HttpWebResponse)nextRequest.GetResponse();
+                    res = nextResponse.ProtocolVersion.ToString();
+                    StreamReader reader = new StreamReader(nextResponse.GetResponseStream());
+                    var result = reader.ReadToEnd();
 
-            editName.Text = "";
-            editSchool.Text = "";
-            autoCompleteCountry.Text = "";
-            gender.Check(0);
+                    using JsonDocument doc = JsonDocument.Parse(result);
+                    JsonElement root = doc.RootElement;
+                    var searchedElement = root[0];
+                    string searchedName = searchedElement.GetProperty("name").ToString();
+                    Toast.MakeText(this, "Student Name '" + name + "' already exists!", ToastLength.Long).Show();
+                }
+                catch (Exception)
+                {
+                    nextRequest = (HttpWebRequest)WebRequest.Create("http://192.168.1.130/IT140P/REST/add_record.php?name=" + name + " &school=" + school + " &country=" + country + " &gender=" + selectedGender);
+                    nextResponse = (HttpWebResponse)nextRequest.GetResponse();
+                    StreamReader reader = new StreamReader(nextResponse.GetResponseStream());
+                    res = reader.ReadToEnd();
+                    Toast.MakeText(this, res, ToastLength.Long).Show();
+
+                    editName.Text = "";
+                    editSchool.Text = "";
+                    autoCompleteCountry.Text = "";
+                    gender.Check(0);
+                }             
+            }
         }
 
         public void BackHome(object sender, EventArgs e)
@@ -90,19 +117,21 @@ namespace LabExer5
         public void SearchRecord(object sender, EventArgs e)
         {
             nameToBeSearched = searchName.Text;
+            if (nameToBeSearched == "") { nameToBeSearched = "N/A"; }
+
             nextRequest = (HttpWebRequest)WebRequest.Create("http://192.168.1.130/IT140P/REST/search_record.php?name=" + nameToBeSearched);
             nextResponse = (HttpWebResponse)nextRequest.GetResponse();
             res = nextResponse.ProtocolVersion.ToString();
             StreamReader reader = new StreamReader(nextResponse.GetResponseStream());
             var result = reader.ReadToEnd();
-            Toast.MakeText(this, res, ToastLength.Long).Show();
 
             using JsonDocument doc = JsonDocument.Parse(result);
             JsonElement root = doc.RootElement;
+
             try
             {
                 var searchedElement = root[0];
-
+                int searchedID = Convert.ToInt32(searchedElement.GetProperty("student_ID").ToString());
                 string searchedName = searchedElement.GetProperty("name").ToString();
                 string searchedSchool = searchedElement.GetProperty("school").ToString();
                 string searchedCountry = searchedElement.GetProperty("country").ToString();
@@ -112,32 +141,49 @@ namespace LabExer5
                 editSchool.Text = searchedSchool;
                 autoCompleteCountry.Text = searchedCountry;
                 gender.Check(searchedGender);
+                record_ID = searchedID;
 
                 Toast.MakeText(this, searchedGender.ToString(), ToastLength.Long).Show();
             }
-            catch (Exception)
+            catch(Exception)
             {
                 Toast.MakeText(this, "Data not found!", ToastLength.Long).Show();
             }
         }
 
-        // BUGS ON UPDATE, WILL FIX LATER
+        // LOGIC: Before the user can update a record, the user must search a student record first.
         public void UpdateRecord(object sender, EventArgs e)
         {
-            name = editName.Text;
-            school = editSchool.Text;
-            country = autoCompleteCountry.Text;
+            if (editName.Text == "" ||
+                editSchool.Text == "" ||
+                autoCompleteCountry.Text == "")
+            {
+                Toast.MakeText(this, "FAILED: Search an existing record or fill the missing fields and try again.", ToastLength.Long).Show();
+            }
+            else
+            {
+                try
+                {
+                    name = editName.Text;
+                    school = editSchool.Text;
+                    country = autoCompleteCountry.Text;
 
-            nextRequest = (HttpWebRequest)WebRequest.Create("http://192.168.1.130/IT140P/REST/update_record.php?name=" + name + " &school=" + school + " &country=" + country + " &gender=" + selectedGender);
-            nextResponse = (HttpWebResponse)nextRequest.GetResponse();
-            StreamReader reader = new StreamReader(nextResponse.GetResponseStream());
-            res = reader.ReadToEnd();
-            Toast.MakeText(this, res, ToastLength.Long).Show();
+                    nextRequest = (HttpWebRequest)WebRequest.Create("http://192.168.1.130/IT140P/REST/update_record.php?student_ID=" + record_ID + " &name=" + name + " &school=" + school + " &country=" + country + " &gender=" + selectedGender);
+                    nextResponse = (HttpWebResponse)nextRequest.GetResponse();
+                    StreamReader reader = new StreamReader(nextResponse.GetResponseStream());
+                    res = reader.ReadToEnd();
+                    Toast.MakeText(this, res, ToastLength.Long).Show();
 
-            editName.Text = "";
-            editSchool.Text = "";
-            autoCompleteCountry.Text = "";
-            gender.Check(0);
+                    editName.Text = "";
+                    editSchool.Text = "";
+                    autoCompleteCountry.Text = "";
+                    gender.Check(0);
+                }
+                catch(Exception)
+                {
+                    Toast.MakeText(this, "FAILED: Student data cannot be found.", ToastLength.Long).Show();
+                }
+            }
         }
 
         public void DeleteRecord(object sender, EventArgs e)

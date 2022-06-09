24b2dd1f3c1319b61ba803ec3970d76537ce1616
charmaine <charmaine.rabano@gmail.com>
2022-06-09 16:35:05 +0800

    implemented 5 layouts

---
+++ LabExer5/HomeActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace LabExer5
+{
+    [Activity(Label = "HomeActivity")]
+    public class HomeActivity : Activity
+    {
+        Button btnAdd, btnSearch, btnLogout;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.home_layout);
+
+            btnAdd = FindViewById<Button>(Resource.Id.buttonAdd);
+            btnSearch = FindViewById<Button>(Resource.Id.buttonSearch);
+            btnLogout = FindViewById<Button>(Resource.Id.buttonLogout);
+
+            btnAdd.Click += this.GoToAdd;
+            btnSearch.Click += this.GoToSearch;
+            btnLogout.Click += this.Logout;
+        }
+
+        public void GoToAdd(object sender, EventArgs e)
+        {
+            Intent i = new Intent(this, typeof(AddActivity));
+            StartActivity(i);
+        }
+
+        public void GoToSearch(object sender, EventArgs e)
+        {
+            Intent i = new Intent(this, typeof(SearchActivity));
+            StartActivity(i);
+        }
+
+        public void Logout(object sender, EventArgs e)
+        {
+            Intent i = new Intent(this, typeof(MainActivity));
+            StartActivity(i);
+        }
+    }
+}
---
+++ LabExer5/SearchActivity.cs
@@ -0,0 +1,87 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace LabExer5
+{
+    [Activity(Label = "SearchActivity")]
+    public class SearchActivity : Activity
+    {
+        //readonly string IP_ADDRESS = "192.168.1.130"; //mark
+        readonly string IP_ADDRESS = "192.168.18.4"; //charmaine
+
+        EditText searchName;
+        Button btnSearch, btnHome;
+        HttpWebResponse nextResponse;
+        HttpWebRequest nextRequest;
+        string nameToBeSearched = "", res = "";
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.search_layout);
+
+            searchName = FindViewById<EditText>(Resource.Id.searchName);
+            btnSearch = FindViewById<Button>(Resource.Id.buttonSearch);
+            btnHome = FindViewById<Button>(Resource.Id.buttonHome);
+
+            btnHome.Click += this.BackHome;
+            btnSearch.Click += this.SearchRecord;
+        }
+
+        public void BackHome(object sender, EventArgs e)
+        {
+            Intent i = new Intent(this, typeof(HomeActivity));
+            StartActivity(i);
+        }
+
+        public void SearchRecord(object sender, EventArgs e)
+        {
+            nameToBeSearched = searchName.Text;
+            if (nameToBeSearched == "") { nameToBeSearched = "N/A"; }
+
+            try
+            {
+                nextRequest = (HttpWebRequest)WebRequest.Create("http://" + IP_ADDRESS + "/IT140P/REST/search_record.php?name=" + nameToBeSearched);
+                nextResponse = (HttpWebResponse)nextRequest.GetResponse();
+                res = nextResponse.ProtocolVersion.ToString();
+                StreamReader reader = new StreamReader(nextResponse.GetResponseStream());
+                var result = reader.ReadToEnd();
+
+                using JsonDocument doc = JsonDocument.Parse(result);
+                JsonElement root = doc.RootElement;
+
+                var searchedElement = root[0];
+                int searchedID = Convert.ToInt32(searchedElement.GetProperty("student_ID").ToString());
+                string searchedName = searchedElement.GetProperty("name").ToString();
+                string searchedSchool = searchedElement.GetProperty("school").ToString();
+                string searchedCountry = searchedElement.GetProperty("country").ToString();
+                int searchedGender = Convert.ToInt32(searchedElement.GetProperty("gender").ToString());
+
+                Toast.MakeText(this, searchedGender.ToString(), ToastLength.Long).Show();
+
+                Intent i = new Intent(this, typeof(ViewActivity));
+                i.PutExtra("Name", searchedName);
+                i.PutExtra("School", searchedSchool);
+                i.PutExtra("Country", searchedCountry);
+                i.PutExtra("Gender", searchedGender);
+                i.PutExtra("ID", searchedID);
+                StartActivity(i);
+            }
+            catch (Exception)
+            {
+                Toast.MakeText(this, "Data not found!", ToastLength.Long).Show();
+            }
+        }
+    }
+}
